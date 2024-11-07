0888bf060b378588e837b9f13dc943362eb3462b
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-11-06 21:21:08 -0300

    :bookmark: Continuação da implementação do autômato

--- 1°Trabalho/src/AutomatoLexico.py
+++ 1°Trabalho/src/AutomatoLexico.py
@@ -12,18 +12,59 @@ class AutomatoLexico:
         self.reserveWords = Token.palavras_reservadas()
         self.symbols = Token.simbolos_especiais()
 
+    def cria_token(self, tipoToken, inicio, fim):
+        lexema = Lexema(inicio, fim)
+        token = Token(tipoToken, lexema, self.linha_atual)
+        self.tokens.append(token)
+        self.estado = 0
+        self.lexema = []
+
     def processar_caractere(self, caractere:str):
         if self.estado == 0:
             if caractere in self.symbols:
                 self.inicio_lexema = self.posicao_atual
                 tipoToken = self.symbols[caractere]
-                lexema = Lexema(self.inicio_lexema, self.posicao_atual)
-                token = Token(tipoToken, lexema, self.linha_atual)
-                self.tokens.append(token)
+                self.cria_token(tipoToken, self.inicio_lexema, self.posicao_atual)
+            elif caractere == '=':
+                self.lexema.append(caractere)
+                self.inicio_lexema = self.posicao_atual
+                self.estado = 1
+            elif caractere == '!':
+                self.lexema.append(caractere)
+                self.inicio_lexema = self.posicao_atual
+                self.estado = 2
+            elif caractere == '>':
+                self.lexema.append(caractere)
+                self.inicio_lexema = self.posicao_atual
+                self.estado = 3
             elif caractere == ' ':
+                self.lexema = []
                 pass
             elif caractere == '\n':
                 self.linha_atual += 1
                 self.posicao_atual = 0
+        elif self.estado == 1:
+            if caractere == '=':
+                self.lexema.append(caractere)
+                tipoToken = 'EQ'
+                self.cria_token(tipoToken, self.inicio_lexema, self.posicao_atual) 
+            elif caractere is not '=':
+                self.lexema.append(caractere)
+                tipoToken = 'ASSIGN'
+                self.cria_token(tipoToken, self.inicio_lexema, self.posicao_atual)
+        elif self.estado == 2:
+            if caractere == '=':
+                self.lexema.append(caractere)
+                tipoToken = 'NE'
+                self.cria_token(tipoToken, self.inicio_lexema, self.posicao_atual)
+        elif self.estado == 3:
+            if caractere == '=':
+                self.lexema.append(caractere)
+                tipoToken = 'GE'
+                self.cria_token(tipoToken, self.inicio_lexema, self.posicao_atual)
+            elif caractere is not '=':
+                self.lexema.append(caractere)
+                tipoToken = 'GT'
+                self.cria_token(tipoToken, self.inicio_lexema, self.posicao_atual)
         self.posicao_atual += 1
                 
